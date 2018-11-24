a0bc9593ea12603b49fc743fd5e3725f5f6c2f15
Patrick Klaeren <patrick@totalren.com>
2018-11-24 20:42:00 +0000

    Added initial Discord bot framework

---
+++ src/Disbot/Modules/PingModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using JetBrains.Annotations;
+
+namespace Disbot.Modules
+{
+    [Group("ping")]
+    [UsedImplicitly]
+    public class PingModule : ModuleBase
+    {
+        [Command, Summary("Pings the bot and sends a response, if the bot is alive!")]
+        [UsedImplicitly]
+        public Task Ping()
+        {
+            return ReplyAsync("I am... ALIVE!");
+        }
+    }
+}
