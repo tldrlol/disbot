0dcb9054740a30adc128bdd960aed7cc31a0785c
Patrick Klaeren <patrick@totalren.com>
2018-12-14 19:59:29 +0000

    Add try/catch handling for reddit feed module

--- src/Disbot/Modules/RedditFeedModule.cs
+++ src/Disbot/Modules/RedditFeedModule.cs
@@ -8,6 +8,7 @@ using Discord;
 using Discord.Commands;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
+using Serilog;
 
 namespace Disbot.Modules
 {
@@ -41,31 +42,38 @@ namespace Disbot.Modules
                 subreddit = subreddit.Replace("r/", string.Empty);
             }
 
-            using (var client = new HttpClient())
+            try
             {
-                var response = await client.GetAsync(string.Format(REDDIT_URI, subreddit));
-
-                var content = await response.Content.ReadAsStringAsync();
-
-                var deserialized = JsonConvert.DeserializeObject<RedditModel>(content);
-
-                if (deserialized?.Data?.Posts == null || deserialized.Data.Posts.Any() == false)
+                using (var client = new HttpClient())
                 {
-                    await ReplyAsync($"Couldn't find anything on /r/{subreddit} :(");
-                    return;
+                    var response = await client.GetAsync(string.Format(REDDIT_URI, subreddit));
+
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    var deserialized = JsonConvert.DeserializeObject<RedditModel>(content);
+
+                    if (deserialized?.Data?.Posts == null || deserialized.Data.Posts.Any() == false)
+                    {
+                        await ReplyAsync($"Couldn't find anything on /r/{subreddit} :(");
+                        return;
+                    }
+
+                    foreach (var post in deserialized.Data.Posts.Where(x => x.Data.IsMeta == false).Take(MAX_ITEMS_TO_TAKE))
+                    {
+                        var embed = new EmbedBuilder().AddInlineField(post.Data.Title.Truncate(256, true), post.Data.Url);
+
+                        if (!string.IsNullOrWhiteSpace(post.Data.Thumbnail) && Uri.IsWellFormedUriString(post.Data.Thumbnail, UriKind.Absolute))
+                            embed.WithThumbnailUrl(post.Data.Thumbnail);
+
+                        embed.WithFooter($"Posted by {post.Data.Author} | {post.Data.Upvotes} 👍");
+
+                        await ReplyAsync(string.Empty, embed: embed);
+                    }
                 }
-
-                foreach (var post in deserialized.Data.Posts.Where(x => x.Data.IsMeta == false).Take(MAX_ITEMS_TO_TAKE))
-                {
-                    var embed = new EmbedBuilder().AddInlineField(post.Data.Title.Truncate(256, true), post.Data.Url);
-
-                    if (!string.IsNullOrWhiteSpace(post.Data.Thumbnail) && Uri.IsWellFormedUriString(post.Data.Thumbnail, UriKind.Absolute))
-                        embed.WithThumbnailUrl(post.Data.Thumbnail);
-
-                    embed.WithFooter($"Posted by {post.Data.Author} | {post.Data.Upvotes} 👍");
-
-                    await ReplyAsync(string.Empty, embed: embed);
-                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Failed getting reddit feed for subreddit {subreddit}", subreddit);
             }
         }
     }
